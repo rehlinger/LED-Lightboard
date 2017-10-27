409a52a17f64175deb835be35bf4f32382884f67
Alex <rehlinger@outlook.com>
2017-10-27 14:32:25 -0400

    cleanup

--- LED_Display_Bluetooth_Controlled.ino
+++ LED_Display_Bluetooth_Controlled.ino
@@ -3,29 +3,31 @@
 #include <Adafruit_NeoPixel.h>
 #include "Alex.h"
 #include <SoftwareSerial.h>
+#define LEDx 32
+#define LEDy 8
 #define PIN 3
 
-Adafruit_NeoMatrix LED = Adafruit_NeoMatrix(32, 8, PIN,
+Adafruit_NeoMatrix LED = Adafruit_NeoMatrix(LEDx, LEDy, PIN,
   NEO_MATRIX_BOTTOM  + NEO_MATRIX_RIGHT +
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
   NEO_GRB            + NEO_KHZ800);// init LED setup
 
-#define timesRepeated 2// number of additional repeats
-int displayLength = 25;// length of desired screen pixels
-int startingOffset = 3;// screen offset from matrix 0 position
-String txt = "HUMAN";// default printed text
-int txtLength = txt.length()*6;
-int trailingOffset = 5; 
 int BLUETOOTH_TX = 7;// connection of Arduino dpin to TX and RX of the Bluetooth module.
 int BLUETOOTH_RX = 8; 
 SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
-int ledpin = 5;// led on D5 will start off
 int BluetoothData;// received data
 char temp;
 String inputString="";
 String sentTxt="";
+
+int startingOffset = 9;// screen offset from max LED position (L->R) [0-x]
+int displayLength = 19;// length of desired screen pixels
+int trailingOffset = 5; 
+const int timesRepeated = 3;// max number of additional repeats needed in txt loop for screen size
 int from0[timesRepeated+1] = {};// distance from desired 0 pos
 int nextColorQueue[timesRepeated+1] = {};// display color
+String txt = "HUMAN";// default printed text
+int txtLength = txt.length()*6;
 
 const uint16_t colors[] = {
   LED.Color(255, 0, 0), LED.Color(0, 255, 255), LED.Color(255, 0, 255), 
@@ -38,7 +40,6 @@ void setup() {
   LED.setFont(&DejaVu_Sans_7);
   bluetooth.begin(9600);
   Serial.println("Enter Text:");
-  pinMode(ledpin, OUTPUT);  
   randomSeed(analogRead(0));
   //Setup variable arrays with initial positions/settings
   for (int i = 0; i <= timesRepeated; i++) {
@@ -54,7 +55,7 @@ void loop() {
 
   for (int i = 0; i <= timesRepeated; i++) {
     LED.setTextColor(colors[nextColorQueue[i]]);
-    LED.setCursor(startingOffset+from0[i], 5);
+    LED.setCursor(startingOffset+from0[i], 6);
     LED.print(txt);
     
     if(--from0[i] <= -txtLength-trailingOffset) {// position is offscreen
@@ -65,16 +66,16 @@ void loop() {
     }
   }
   
-  //Disregard Columns 1-4 & 30-32
-  for (int i = 0; i<4*8; i++) {
+  //Disregard Unused Columns 
+  for (int i = 0; i<(LEDx-(displayLength+startingOffset))*LEDy; i++) {// right of words
   LED.setPixelColor( i, LED.Color( 0, 0, 0 ) );
   }
-  for (int i = 32*8-1; i>29*8-1; i--) {
-  LED.setPixelColor( i, LED.Color( 0, 0, 0 ) );
+  for (int i = LEDx*LEDy; i>(LEDx-startingOffset)*LEDy; i--) {// left of words
+  LED.setPixelColor( i-1, LED.Color( 0, 0, 0 ) );
   }
   
   LED.show();    
-  delay(displayLength*1.5);// scrolling speed
+  delay(displayLength*3);// scrolling speed
   
   if(bluetooth.available()){
     while(bluetooth.available()) {
@@ -84,11 +85,7 @@ void loop() {
     bluetooth.println("Sent: "+inputString);
     while (bluetooth.available() > 0) {
       temp = bluetooth.read();}// clearing the serial buffer
-    if(inputString == "a"){//turn the LED on/off from chars
-      digitalWrite(5, HIGH);  
-    }else if(inputString == "b"){
-      digitalWrite(5, LOW);
-    }else if(inputString != "") {
+    if(inputString != "") {
       LED.fillScreen(0);
       txt = inputString;
       txtLength    = txt.length()*6;
